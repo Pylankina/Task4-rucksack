73dcaee160a4fc644525e73f6803b7e86ea0e9e3
Pylankina <126517247+Pylankina@users.noreply.github.com>
2024-08-20 11:07:31 +0300

    Solver.cpp

--- Solver.cpp
+++ Solver.cpp
@@ -30,6 +30,7 @@ Solver::Solver(ifstream &fin):DB(fin){
     a.P=0;
     a.W=0;
     a.lim=bound(a);
+    a.taken.resize(n, false); // инициализируем вектор для хранения статуса предметов
     q.push_back(a);//добавили корень в очередь
     node b;
     while(!q.empty()){
@@ -41,8 +42,11 @@ Solver::Solver(ifstream &fin):DB(fin){
             // Состояние, когда элемент b.l добавляется в рюкзак
             b.W = a.W + w[b.l].second;
             b.P = a.P + w[b.l].first;
+            b.taken = a.taken; // копируем информацию о взятых предметах
+            b.taken[b.l] = true;
             if (b.W <= K && b.P > max) 
                 max = b.P; 
+                answer = b.taken; // сохраним текущее решение
             
             // вычисляем верхнюю границу для включенного элемента
             b.lim=bound(b);
@@ -55,6 +59,7 @@ Solver::Solver(ifstream &fin):DB(fin){
             // Состояние, когда элемент b.l не добавляется в рюкзак
             b.W = a.W;
             b.P = a.P;
+            b.taken = a.taken; // копируем состояние без использования предмета
             b.lim= bound(b);
             if (b.lim>max)
                 q.push_back(b); // добавляем в очередь, если превышает max
