59cc851b1454eaf0789a344c06587285daa391c5
Pylankina <126517247+Pylankina@users.noreply.github.com>
2024-08-20 11:05:35 +0300

    DB.cpp

--- DB.cpp
+++ DB.cpp
@@ -6,19 +6,25 @@ DB::DB(ifstream &fin): Parser(fin){
     int i;
     for (i=0; i<w.size(); i++)
         z.push_back(make_pair(i, 0.0));//инициализация вектора удельной стоимости
-    max=0;
-    for (i=0; i<n; i++)
+    max=res=0;
+    for (i=0; i<n; i++)//инициализация вектора ответа
         answer.push_back(0);
 };
 
+void DB::function(){
+    for (int i=0; i<n; i++)
+        res=res+answer[i]*w[i].second;
+    return;
+}
 
 void DB::writting(){ // функция записи ответа в файл
     ofstream out;
     out.open("answer.txt");
+    function();
     if (out.is_open()){
-        out<< "Максимальная ценность в итоге:" << max <<endl;
-       for (const auto& item : ans) 
-        out << "Вещь: " << item.first << ", В рюкзаке?: " << item.second << endl;
+        out<< max << " " << K-res <<endl;
+        for (int i=0; i<n; i++) 
+            out << answer[i] << " ";
     }
     out.close();
 }
